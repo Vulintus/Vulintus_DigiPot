8d02d405c0b1b52a0e0ce06db779847f11919dca
Drew Sloan <drew@vulintus.com>
2023-12-12 21:49:08 -0700

    Added support for multiple SPI buses.

--- Vulintus_MCP4xxx_DigPot.cpp
+++ Vulintus_MCP4xxx_DigPot.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
     Vulintus_MCP4xxx_DigPot.cpp
 
     Copyright 2023, Vulintus, Inc.
@@ -24,30 +24,44 @@
 */
 
 
-#include <Vulintus_MCP4xxx_DigPot.h>    //Vulintus MCP4xxx digital potentiometer library.     
-#include <SPI.h>                        //Arduino SPI library.
+#include <Vulintus_MCP4xxx_DigPot.h>    // Vulintus MCP4xxx digital potentiometer library.     
 
 
 // CLASS FUNCTIONS ***********************************************************// 
 
-// Class constructor.
+// Class constructor (default SPI with chip select).
 Vulintus_MCP4xxx_DigPot::Vulintus_MCP4xxx_DigPot(uint8_t pin_cs)
     : _pin_cs(pin_cs)
 {
-    SPI.begin();                        //Initialize SPI communication.
-    pinMode(_pin_cs, OUTPUT);            //Set the CS pin mode to output.
-    digitalWrite(_pin_cs, HIGH);         //Set the CS pin high.
+    _spi_bus = &SPI;                    // Set the SPI bus to the default bus.
 }
 
 
-//Read the Wiper 0 value.
+// Class constructor (specified SPI with chip select).
+Vulintus_MCP4xxx_DigPot::Vulintus_MCP4xxx_DigPot(SPIClass *spi_bus, uint8_t pin_cs)
+    : _pin_cs(pin_cs)
+{
+    _spi_bus = spi_bus;                 // Set the SPI bus to the specified bus.
+}
+
+
+// Initialization.
+void Vulintus_MCP4xxx_DigPot::begin(void)
+{
+    _spi_bus->begin();                  // Initialize the SPI bus.
+    pinMode(_pin_cs, OUTPUT);           // Set the CS pin mode to output.
+    digitalWrite(_pin_cs, HIGH);        // Set the CS pin high.
+}
+
+
+// Read the Wiper 0 value.
 uint16_t Vulintus_MCP4xxx_DigPot::read()
 {
-    return read((uint8_t) 0);             //Read the value from wiper 0.
+    return read((uint8_t) 0);           // Read the value from wiper 0.
 }                         
 
 
-//Read the specified wiper value.
+// Read the specified wiper value.
 uint16_t Vulintus_MCP4xxx_DigPot::read(uint8_t wiper_i)
 {
     if (!wiper_i) {
@@ -59,14 +73,14 @@ uint16_t Vulintus_MCP4xxx_DigPot::read(uint8_t wiper_i)
 }          
 
 
-//Write the Wiper 0 value.
+// Write the Wiper 0 value.
 void Vulintus_MCP4xxx_DigPot::write(uint16_t value)
 {
-    write(value, (uint8_t) 0);   //Write the value to wiper 0.
+    write(value, (uint8_t) 0);          // Write the value to wiper 0.
 }     
 
 
-//Write the specified wiper value.
+// Write the specified wiper value.
 void Vulintus_MCP4xxx_DigPot::write(uint16_t value, uint8_t wiper_i)
 {
     if (!wiper_i) {
@@ -78,14 +92,14 @@ void Vulintus_MCP4xxx_DigPot::write(uint16_t value, uint8_t wiper_i)
 }   
 
 
-//Increment Wiper 0.MCP4XXX
+// Increment Wiper 0.MCP4XXX
 void Vulintus_MCP4xxx_DigPot::increment()
 {
-    increment((uint8_t) 0);     //Increment wiper 0.
+    increment((uint8_t) 0);             // Increment wiper 0.
 }           
 
 
-//Increment the specified wiper.
+// Increment the specified wiper.
 void Vulintus_MCP4xxx_DigPot::increment(uint8_t wiper_i)
 {
     if (!wiper_i) {
@@ -97,14 +111,14 @@ void Vulintus_MCP4xxx_DigPot::increment(uint8_t wiper_i)
 }            
 
 
-//Increment Wiper 1.
+// Increment Wiper 1.
 void Vulintus_MCP4xxx_DigPot::decrement()
 {
-    decrement((uint8_t) 0);     //Decrement wiper 0.
+    decrement((uint8_t) 0);             // Decrement wiper 0.
 }                         
 
 
-//Increment the specified wiper.
+// Increment the specified wiper.
 void Vulintus_MCP4xxx_DigPot::decrement(uint8_t wiper_i)
 {
     if (!wiper_i) {
@@ -116,34 +130,34 @@ void Vulintus_MCP4xxx_DigPot::decrement(uint8_t wiper_i)
 }           
 
 
-//Send a command with data.
+// Send a command with data.
 uint16_t Vulintus_MCP4xxx_DigPot::send_cmd(uint8_t addr, uint8_t cmd, uint16_t data)
 {
-    uint8_t hi_byte = addr | cmd | (data >> 8);     //Combine the address, command and MSB to make the high byte.
-    uint8_t lo_byte = data;                         //Grab the bottom 8 bits from the data for the low byte.
+    uint8_t hi_byte = addr | cmd | (data >> 8);     // Combine the address, command and MSB to make the high byte.
+    uint8_t lo_byte = data;                         // Grab the bottom 8 bits from the data for the low byte.
 
-    SPI.beginTransaction(SPISettings(250000, MSBFIRST, SPI_MODE0)); //Set the SPI settings for this chip.
-    digitalWrite(_pin_cs, LOW);                                     //Set the chip select line low.    
-    hi_byte = SPI.transfer(hi_byte);                                //Send the high byte.
-    lo_byte = SPI.transfer(lo_byte);                                //Send the low byte.
-    digitalWrite(_pin_cs, HIGH);                                    //Set the chip select line high. 
-    SPI.endTransaction();                                           //Release the SPI bus.
+    _spi_bus->beginTransaction(SPISettings(250000, MSBFIRST, SPI_MODE0));   // Set the SPI settings for this chip.
+    digitalWrite(_pin_cs, LOW);                                             // Set the chip select line low.    
+    hi_byte = _spi_bus->transfer(hi_byte);                                  // Send the high byte.
+    lo_byte = _spi_bus->transfer(lo_byte);                                  // Send the low byte.
+    digitalWrite(_pin_cs, HIGH);                                            // Set the chip select line high. 
+    _spi_bus->endTransaction();                                             // Release the SPI bus.
 
-    uint16_t reply = (uint16_t) (hi_byte << 8) + lo_byte;           //Combine the high and low bytes.
-    reply &= 0x01FF;                                                //Kick out all but the bottom 9 bits.
+    uint16_t reply = (uint16_t) (hi_byte << 8) + lo_byte;   // Combine the high and low bytes.
+    reply &= 0x01FF;                                        // Kick out all but the bottom 9 bits.
 
-    return reply;                                                   //Return the result.
+    return reply;                                           // Return the result.
 }
 
 
-//Send a command without data (increment, decrement).
+// Send a command without data (increment, decrement).
 void Vulintus_MCP4xxx_DigPot::send_cmd(uint8_t addr, uint8_t cmd)
 {
-    uint8_t hi_byte = addr | cmd;           //Combine the address, command and MSB to make the high byte.
+    uint8_t hi_byte = addr | cmd;           // Combine the address, command and MSB to make the high byte.
 
-    SPI.beginTransaction(SPISettings(250000, MSBFIRST, SPI_MODE0)); //Set the SPI settings for this chip.
-    digitalWrite(_pin_cs, LOW);                                     //Set the chip select line low.    
-    SPI.transfer(hi_byte);                                          //Send the high byte.
-    digitalWrite(_pin_cs, HIGH);                                    //Set the chip select line high. 
-    SPI.endTransaction();                                           //Release the SPI bus.
+    _spi_bus->beginTransaction(SPISettings(250000, MSBFIRST, SPI_MODE0));   // Set the SPI settings for this chip.
+    digitalWrite(_pin_cs, LOW);                                             // Set the chip select line low.    
+    _spi_bus->transfer(hi_byte);                                            // Send the high byte.
+    digitalWrite(_pin_cs, HIGH);                                            // Set the chip select line high. 
+    _spi_bus->endTransaction();                                             // Release the SPI bus.
 }                
--- Vulintus_MCP4xxx_DigPot.h
+++ Vulintus_MCP4xxx_DigPot.h
@@ -11,7 +11,7 @@
 
     You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+    http:// www.apache.org/licenses/LICENSE-2.0
 
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
@@ -28,10 +28,9 @@
 #define VULINTUS_MCP4XXX_H
 
 
-//Included libraries.//
+// Included libraries.// 
 #include <Arduino.h>            // Arduino main header.
-#include <Wire.h>               // Arduino I2C library.
-
+#include <SPI.h>		        // Standard Arduino SPI library.
 
 
 // CLASS *************************************************************************************************************// 
@@ -39,49 +38,52 @@ class Vulintus_MCP4xxx_DigPot {
 
 	public:
 
-		// Constructor. //
-		Vulintus_MCP4xxx_DigPot(uint8_t pin_cs);
+		// Constructor. // 
+		Vulintus_MCP4xxx_DigPot(uint8_t pin_cs);                        // Default SPI with chip select.
+        Vulintus_MCP4xxx_DigPot(SPIClass *spi_bus, uint8_t pin_cs);     // Specified SPI with chip select.
 
-        // Functions. //
-        uint16_t read();                                //Read the Wiper 0 value.
-        uint16_t read(uint8_t wiper_i);                 //Read the specified wiper value.
-        void write(uint16_t value);                     //Write the Wiper 0 value.
-        void write(uint16_t value, uint8_t wiper_i);    //Write the specified wiper value.MCP4XXX
-        void increment();                               //Increment Wiper 0.
-        void increment(uint8_t wiper_i);                //Increment the specified wiper.
-        void decrement();                               //Increment Wiper 1.
-        void decrement(uint8_t wiper_i);                //Increment the specified wiper.
+        // Functions. // 
+        void begin(void); 							    // Initialization.
+        uint16_t read();                                // Read the Wiper 0 value.
+        uint16_t read(uint8_t wiper_i);                 // Read the specified wiper value.
+        void write(uint16_t value);                     // Write the Wiper 0 value.
+        void write(uint16_t value, uint8_t wiper_i);    // Write the specified wiper value.MCP4XXX
+        void increment();                               // Increment Wiper 0.
+        void increment(uint8_t wiper_i);                // Increment the specified wiper.
+        void decrement();                               // Increment Wiper 1.
+        void decrement(uint8_t wiper_i);                // Increment the specified wiper.
 
     private:
 
-        // Constants. //
-        static const uint8_t MCP4XXX_REG_WIPER0  = 0x00;            //Volatile Wiper 0.
-        static const uint8_t MCP4XXX_REG_WIPER1  = 0x10;            //Volatile Wiper 1.
-        static const uint8_t MCP4XXX_REG_TCON    = 0x40;            //Volatile TCON Register.
-        static const uint8_t MCP4XXX_REG_STATUS  = 0x50;            //Status Register.
+        // Constants. // 
+        static const uint8_t MCP4XXX_REG_WIPER0  = 0x00;            // Volatile Wiper 0.
+        static const uint8_t MCP4XXX_REG_WIPER1  = 0x10;            // Volatile Wiper 1.
+        static const uint8_t MCP4XXX_REG_TCON    = 0x40;            // Volatile TCON Register.
+        static const uint8_t MCP4XXX_REG_STATUS  = 0x50;            // Status Register.
 
-        static const uint8_t MCP4XXX_STATUS_SHDN = 0x02;            //Hardware Shutdown pin Status bit.
+        static const uint8_t MCP4XXX_STATUS_SHDN = 0x02;            // Hardware Shutdown pin Status bit.
 
-        static const uint8_t MCP4XXX_TCON_R0HW   = 0x08;            //Resistor 0 Hardware Configuration Control bit.
-        static const uint8_t MCP4XXX_TCON_R0A    = 0x04;            //Resistor 0 Terminal A (P0A pin) Connect Control bit .
-        static const uint8_t MCP4XXX_TCON_R0W    = 0x02;            //Resistor 0 Wiper (P0W pin) Connect Control bit.
-        static const uint8_t MCP4XXX_TCON_R0B    = 0x01;            //Resistor 0 Terminal B (P0B pin) Connect Control bit.
-        static const uint8_t MCP4XXX_TCON_R1HW   = 0x80;            //Resistor 1 Hardware Configuration Control bit.
-        static const uint8_t MCP4XXX_TCON_R1A    = 0x40;            //Resistor 1 Terminal A (P1A pin) Connect Control bit .
-        static const uint8_t MCP4XXX_TCON_R1W    = 0x20;            //Resistor 1 Wiper (P1W pin) Connect Control bit.
-        static const uint8_t MCP4XXX_TCON_R1B    = 0x10;            //Resistor 1 Terminal B (P1B pin) Connect Control bit.
+        static const uint8_t MCP4XXX_TCON_R0HW   = 0x08;            // Resistor 0 Hardware Configuration Control bit.
+        static const uint8_t MCP4XXX_TCON_R0A    = 0x04;            // Resistor 0 Terminal A (P0A pin) Connect Control bit .
+        static const uint8_t MCP4XXX_TCON_R0W    = 0x02;            // Resistor 0 Wiper (P0W pin) Connect Control bit.
+        static const uint8_t MCP4XXX_TCON_R0B    = 0x01;            // Resistor 0 Terminal B (P0B pin) Connect Control bit.
+        static const uint8_t MCP4XXX_TCON_R1HW   = 0x80;            // Resistor 1 Hardware Configuration Control bit.
+        static const uint8_t MCP4XXX_TCON_R1A    = 0x40;            // Resistor 1 Terminal A (P1A pin) Connect Control bit .
+        static const uint8_t MCP4XXX_TCON_R1W    = 0x20;            // Resistor 1 Wiper (P1W pin) Connect Control bit.
+        static const uint8_t MCP4XXX_TCON_R1B    = 0x10;            // Resistor 1 Terminal B (P1B pin) Connect Control bit.
 
-        static const uint8_t MCP4XXX_CMD_WRITE   = 0x00;            //Write data command.
-        static const uint8_t MCP4XXX_CMD_READ    = 0x0C;            //Read data command.
-        static const uint8_t MCP4XXX_CMD_INCR    = 0x04;            //Increment command.
-        static const uint8_t MCP4XXX_CMD_DECR    = 0x08;            //Decrement command.
+        static const uint8_t MCP4XXX_CMD_WRITE   = 0x00;            // Write data command.
+        static const uint8_t MCP4XXX_CMD_READ    = 0x0C;            // Read data command.
+        static const uint8_t MCP4XXX_CMD_INCR    = 0x04;            // Increment command.
+        static const uint8_t MCP4XXX_CMD_DECR    = 0x08;            // Decrement command.
 
-        // Variables. //
-        const uint8_t _pin_cs;                                      //Chip select pin.
+        // Variables. // 
+        SPIClass *_spi_bus = NULL;	    // SPI interface pointer.
+        const uint8_t _pin_cs;          // Chip select pin.
 
-        // Private Functions. //
-        uint16_t send_cmd(uint8_t addr, uint8_t cmd, uint16_t data);     //Send a command with data.
-        void send_cmd(uint8_t addr, uint8_t cmd);                        //Send a command without data (increment, decrement).
+        // Private Functions. // 
+        uint16_t send_cmd(uint8_t addr, uint8_t cmd, uint16_t data);     // Send a command with data.
+        void send_cmd(uint8_t addr, uint8_t cmd);                        // Send a command without data (increment, decrement).
 
 };
 
