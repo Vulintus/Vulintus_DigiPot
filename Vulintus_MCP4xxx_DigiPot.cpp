3bae121333a12c848c8c778c38150dde4ecce685
Drew Sloan <drew@vulintus.com>
2024-01-20 22:48:15 -0700

    'paragraph' error fix.

--- Vulintus_MCP4xxx_DigiPot.cpp
+++ Vulintus_MCP4xxx_DigiPot.cpp
@@ -136,7 +136,6 @@ uint16_t Vulintus_MCP4xxx_DigiPot::send_cmd(uint8_t addr, uint8_t cmd, uint16_t 
     uint8_t hi_byte = addr | cmd | (data >> 8);     // Combine the address, command and MSB to make the high byte.
     uint8_t lo_byte = data;                         // Grab the bottom 8 bits from the data for the low byte.
 
-    Serial.print()
     _spi_bus->beginTransaction(SPISettings(250000, MSBFIRST, SPI_MODE0));   // Set the SPI settings for this chip.
     digitalWrite(_pin_cs, LOW);                                             // Set the chip select line low.    
     hi_byte = _spi_bus->transfer(hi_byte);                                  // Send the high byte.
